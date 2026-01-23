a8fb19adcf5e80bccd2636572fb3e3d4f4c9f1e1
hagyutae <gted221@gmail.com>
2026-01-23 15:45:03 +0900

    Fix JpaIncidentTypeRepository method naming to match Spring Data JPA conventions
    Change findByCodes() to findByCodeIn() to follow Spring Data JPA standard naming conventions for Collection parameters.

--- persistence/src/main/kotlin/com/vonkernel/lit/persistence/jpa/JpaIncidentTypeRepository.kt
+++ persistence/src/main/kotlin/com/vonkernel/lit/persistence/jpa/JpaIncidentTypeRepository.kt
@@ -4,5 +4,5 @@ import com.vonkernel.lit.persistence.entity.core.IncidentTypeEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface JpaIncidentTypeRepository : JpaRepository<IncidentTypeEntity, Long> {
-    fun findByCodes(codes: Collection<String>): List<IncidentTypeEntity>
+    fun findByCodeIn(codes: Collection<String>): List<IncidentTypeEntity>
 }
